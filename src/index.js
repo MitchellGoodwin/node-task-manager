195a70d998121b8716b6bdd5432ddcda018f9db8
Mitchell Goodwin <mgoodwin777@gmail.com>
2020-05-08 13:39:31 -0500

    Updating Users Added

--- src/index.js
+++ src/index.js
@@ -43,6 +43,33 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!'})
+    }
+
+    const _id = req.params.id
+
+    try {
+        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        
+        res.send(user)
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
@@ -68,6 +95,21 @@ app.get('/tasks/:id', async (req, res) => {
 
     try {
         const task = await Task.findById(_id)
+
+        if (!task) {
+            return res.status(400).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
 
         if (!task) {
             return res.status(400).send()
