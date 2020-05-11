bd841469a706870b9645810fb807d5a0904c25d5
Mitchell Goodwin <mgoodwin777@gmail.com>
2020-05-11 15:54:19 -0500

    Added my profile route

--- src/models/user.js
+++ src/models/user.js
@@ -54,7 +54,7 @@ userSchema.methods.generateAuthToken = async function () {
     const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
 
     user.tokens = user.tokens.concat({ token })
-    await user.save
+    await user.save()
 
     return token
 }
