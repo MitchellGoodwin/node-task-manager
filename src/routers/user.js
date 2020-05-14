5ad98a235633bc3780747e7fb64ec2548bd7260d
Mitchell Goodwin <mgoodwin777@gmail.com>
2020-05-14 16:02:59 -0500

    Automatic image formating

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = new express.Router()
 const auth = require('../middleware/auth')
+const sharp = require('sharp')
 const User = require('../models/user')
 const multer = require('multer')
 const upload = multer({
@@ -104,7 +105,8 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
 }, (error, req, res, next) => {
@@ -125,7 +127,7 @@ router.get('/users/:id/avatar', async (req, res) => {
             throw new Error()
         }
 
-        res.set('Content-Type', 'image/jpg')
+        res.set('Content-Type', 'image/png')
         res.send(user.avatar)
     } catch(e) {
         res.status(404).send()
