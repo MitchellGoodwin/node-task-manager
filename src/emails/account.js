a19c8321fecd82444c4cb7f10f44714bb7f560aa
Mitchell Goodwin <mgoodwin777@gmail.com>
2020-05-18 11:17:33 -0500

    Created sending emails for signup and cancellation

--- src/emails/account.js
+++ src/emails/account.js
@@ -5,9 +5,28 @@ const address = process.env.EMAIL_ADDRESS
 
 sgMail.setApiKey(sendgridAPIKey)
 
-sgMail.send({
-    to: address,
-    from: address,
-    subject: 'This is my first creation',
-    text: 'I hope this actually gets to you'
-})
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: address,
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    }).catch(e => {
+        console.log(e)
+        console.log(e.response.body)
+    })
+}
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: address,
+        subject: `Sorry to see you go`,
+        text: `Goodbeye ${name}. Was there anything we could have done to keep you around?`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
